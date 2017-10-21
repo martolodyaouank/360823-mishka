45f63afe2aaf98c2e85f52e37c8a17ea62b8ee20
martolodyaouank <hyuehdgiwc@gmail.com>
2017-10-21 20:30:01 +0300

    Автоматизация сборки

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
 "use strict";
 
-
 var gulp = require("gulp");
 var less = require("gulp-less");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
 var server = require("browser-sync").create();
+var run = require("run-sequence");
+var del = require("del");
+var jsMinify = require("gulp-uglify");
 
 gulp.task("style", function() {
   gulp.src("less/style.less")
@@ -15,13 +24,23 @@ gulp.task("style", function() {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("js", function() {
+  gulp.src("js/script.js")
+    .pipe(jsMinify())
+    .pipe(rename("script.min.js"))
+    .pipe(gulp.dest("build/js"))
+})
+
+gulp.task("serve", function() {
   server.init({
-    server: ".",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -29,5 +48,63 @@ gulp.task("serve", ["style"], function() {
   });
 
   gulp.watch("less/**/*.less", ["style"]);
-  gulp.watch("*.html").on("change", server.reload);
+  gulp.watch("*.html", ["html"]);
+});
+
+gulp.task("images", function() {
+  return gulp.src("build/img/**/*{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({
+        optimizationLevel: 3
+      }),
+      imagemin.jpegtran({
+        progressive: true
+      }),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"))
+});
+
+gulp.task("webp", function() {
+  return gulp.src("build/img/**/*.{png,jpg}")
+    .pipe(webp({
+      quality: 90
+    }))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("sprite", function() {
+  return gulp.src("img/inline-*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("html", function() {
+  return gulp.src("*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+      "fonts/**/*.{woff,woff2}",
+      "img/**",
+      "js/**"
+    ], {
+      base: "."
+    })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("build", function(done) {
+  run("clean", "copy", "images", "webp", "style", "js", "sprite", "html", done);
+});
+
+gulp.task("clean", function() {
+  return del("build");
 });
--- js/script-popup.js
+++ js/script-popup.js
@@ -1,25 +1,40 @@
-var modal = document.querySelector(".goods-popup");
-var button = document.querySelector(".popup-button");
 var overlay = document.querySelector(".goods-popup__overlay");
+var modal = document.querySelector(".goods-popup");
+var goodsPopularButton = document.querySelector(".goods-popular__button");
+var productsButton = document.querySelectorAll(".products__button");
 
 
-button.addEventListener("click", function(event) {
-  event.preventDefault();
-  overlay.classList.add("goods-popup__overlay--show");
-  modal.classList.add("goods-popup--show");
-});
+if (overlay) {
+  for (var i = 0; i < productsButton.length; i++) productsButton[i].addEventListener("click", function(event) {
+    event.preventDefault();
+    overlay.classList.add("goods-popup__overlay--show");
+    modal.classList.add("goods-popup--show");
 
-overlay.addEventListener("click", function(event) {
-  event.preventDefault();
+    window.addEventListener("keydown", function(event) {
+      if (event.keyCode === 27) {
 
-  if (overlay.classList.contains("goods-popup__overlay--show")) {
+        if (overlay.classList.contains("goods-popup__overlay--show")) {
+          overlay.classList.remove("goods-popup__overlay--show");
+        }
+
+        if (modal.classList.contains("goods-popup--show")) {
+          modal.classList.remove("goods-popup--show");
+        }
+      }
+    });
+  });
+
+  overlay.addEventListener("click", function() {
     overlay.classList.remove("goods-popup__overlay--show");
-  }
-
-  if (modal.classList.contains("goods-popup--show")) {
     modal.classList.remove("goods-popup--show");
-  }
-});
+  });
+
+  goodsPopularButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    overlay.classList.add("goods-popup__overlay--show");
+    modal.classList.add("goods-popup--show");
+  });
+}
 
 window.addEventListener("keydown", function(event) {
   if (event.keyCode === 27) {
